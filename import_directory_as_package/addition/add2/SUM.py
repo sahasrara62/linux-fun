afa2b5be756309434844aab01dbe210825dab19c
prashant rana <sahasrara62@gmail.com>
2018-08-05 03:16:56 +0530

    Update SUM.py

--- import_directory_as_package/addition/add2/SUM.py
+++ import_directory_as_package/addition/add2/SUM.py
@@ -1,8 +1,14 @@
 class Add:
+	ans=0
 	def __init__(self):
+		self.reset()
 		pass
 	def sum(self,*args):
 		sol=0
-		for i in args:
-			sol+=i
-		return sol
+		[ self._addition(i) for i in args]
+		return self.ans
+	def _addition(self,i):
+		self.i=i
+		self.ans=self.ans+self.i
+	def reset(self,a=0):
+		self.ans=a
